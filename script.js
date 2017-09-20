d9f7fe6041df0ce3e4f70ac6452d33c4b909b227
Andre Ferreira <aadf.pt@gmail.com>
2017-09-20 13:18:52 +0100

    Adding alternative shortcodes to regexes and monitoring for dom modifications

--- script.js
+++ script.js
@@ -9,40 +9,63 @@
 // @credits Treewalker obtained from https://stackoverflow.com/a/24419809
 // ==/UserScript==
 var replaceArray = [
-    [/00-Secretaria  Lic. Informática/gi, 'Secretaria'],
-    [/00-O Café da Informática/gi, 'Café'],
-    [/00-Coordenação Lic. Inf. - Estudantes/gi, 'Coordenação']
+    [/00-Secretaria  Lic. Informática|2103-sec/gi, 'Secretaria'],
+    [/00-O Café da Informática|2103-cafe/gi, 'Café'],
+    [/00-Coordenação Lic. Inf. - Estudantes|2103-coord/gi, 'Coordenação']
     // etc.
 ];
+
+function SearchAndReplace(target) {
+    var numTerms = replaceArray.length;
+    var txtWalker = document.createTreeWalker(
+        target,
+        NodeFilter.SHOW_TEXT, {
+            acceptNode: function(node) {
+                //-- Skip whitespace-only nodes
+                if (node.nodeValue.trim())
+                    return NodeFilter.FILTER_ACCEPT;
+
+                return NodeFilter.FILTER_SKIP;
+            }
+        },
+        false
+    );
+    var txtNode = null;
+    txtNode = txtWalker.nextNode();
+    while (txtNode) {
+        var oldTxt = txtNode.nodeValue;
+
+        for (var J = 0; J < numTerms; J++) {
+            oldTxt = oldTxt.replace(replaceArray[J][0], replaceArray[J][1]);
+        }
+        txtNode.nodeValue = oldTxt;
+        txtNode = txtWalker.nextNode();
+    }
+}
 (function() {
     'use strict';
     $('body').hide();
     $(document).ready(function() {
-        var numTerms = replaceArray.length;
-        var txtWalker = document.createTreeWalker(
-            document.body,
-            NodeFilter.SHOW_TEXT, {
-                acceptNode: function(node) {
-                    //-- Skip whitespace-only nodes
-                    if (node.nodeValue.trim())
-                        return NodeFilter.FILTER_ACCEPT;
-
-                    return NodeFilter.FILTER_SKIP;
-                }
-            },
-            false
-        );
-        var txtNode = null;
-        txtNode = txtWalker.nextNode();
-        while (txtNode) {
-            var oldTxt = txtNode.nodeValue;
-
-            for (var J = 0; J < numTerms; J++) {
-                oldTxt = oldTxt.replace(replaceArray[J][0], replaceArray[J][1]);
-            }
-            txtNode.nodeValue = oldTxt;
-            txtNode = txtWalker.nextNode();
-        }
+        SearchAndReplace(document.body);
         $('body').show();
+        var observer = new MutationObserver(function(mutations) {
+            mutations.forEach(function(mutation) {
+                var newNodes = mutation.addedNodes; // DOM NodeList
+                if (newNodes !== null) { // If there are new nodes added
+                    var $nodes = $(newNodes); // jQuery set
+                    $nodes.each(function() {
+                        //var $node = $( this );
+                        SearchAndReplace(this);
+                    });
+                }
+            });
+        });
+        var config = {
+            subtree: true,
+            attributes: false,
+            childList: true,
+            characterData: false
+        };
+        observer.observe(document.body, config);
     });
 })();
