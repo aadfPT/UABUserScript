a9ed7fb508c3dc0d2c7531aa08b49448ef4a1161
Andre Ferreira <aadf.pt@gmail.com>
2017-10-02 13:03:31 +0100

    updating ucs

--- script.js
+++ script.js
@@ -4,6 +4,7 @@
 // @version      0.1
 // @description  Muda os nomes das disciplinas
 // @match        http://elearning.uab.pt/*
+// @exclude        http://elearning.uab.pt/*.pdf
 // @grant        none
 // @require https://code.jquery.com/jquery-latest.js
 // @credits Treewalker obtained from https://stackoverflow.com/a/24419809
@@ -11,6 +12,16 @@
 var replaceArray = [
     [/00-Secretaria  Lic. Informática|2103-sec/gi, 'Secretaria'],
     [/00-O Café da Informática|2103-cafe/gi, 'Café'],
+    [/Ambientação 449|AMB449/gi, 'Ambientação'],
+    [/Arquitectura de Computadores 2017 01|21010_17_01/gi, 'Arquitectura de Computadores'],
+    [/Arquitectura de Computadores \(Espaço Central\)|21010_17_00/gi, 'Arquitectura de Computadores (Central)'],
+    [/Cálculo para Informática 2017 01|21157_17_01/gi, 'Cálculo para Informática'],
+    [/Álgebra Linear I 2017 01|21002_17_01/gi, 'Álgebra Linear I'],
+    [/Álgebra Linear I - Espaço Central 2017|21002_17_00/gi, 'Álgebra Linear I (Central)'],
+    [/LaTeX 2017\/2018|LATEX_17/gi, 'LaTeX 2017/2018'],
+    [/Programação 2017 01|21090_17_01/gi, 'Programação'],
+    [/Programação - Espaço Central|21157_17_01/gi, 'Programação (Central)'],
+    [/Sistemas Multimédia 2017 01|21110_17_01/gi, 'Sistemas Multimédia'],
     [/00-Coordenação Lic. Inf. - Estudantes|2103-coord/gi, 'Coordenação']
     // etc.
 ];
