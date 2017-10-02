9a7f74ab4da64a4d9713c373224d57a58810c4ff
Andre Ferreira <aadf.pt@gmail.com>
2017-10-02 21:53:19 +0100

    optimized regexes and fixed bug in uc programaçao

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Uab Informática
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  Muda os nomes das disciplinas
 // @match        http://elearning.uab.pt/*
 // @exclude        http://elearning.uab.pt/*.pdf
@@ -10,22 +10,22 @@
 // @credits Treewalker obtained from https://stackoverflow.com/a/24419809
 // ==/UserScript==
 var replaceArray = [
-    [/00-Secretaria  Lic. Informática|2103-sec/gi, 'Secretaria'],
-    [/00-O Café da Informática|2103-cafe/gi, 'Café'],
-    [/Ambientação 449|AMB449/gi, 'Ambientação'],
-    [/Arquitectura de Computadores 2017 01|21010_17_0[1-9]/gi, 'Arquitectura de Computadores'],
-    [/Arquitectura de Computadores \(Espaço Central\)|21010_17_00/gi, 'Arquitectura de Computadores (Central)'],
-    [/Cálculo para Informática 2017 01|21157_17_0[1-9]/gi, 'Cálculo para Informática'],
-    [/Álgebra Linear I 2017 01|21002_17_0[1-9]/gi, 'Álgebra Linear I'],
-    [/Álgebra Linear I - Espaço Central 2017|21002_17_0[1-9]/gi, 'Álgebra Linear I (Central)'],
-    [/LaTeX 2017\/2018|LATEX_17/gi, 'LaTeX 2017/2018'],
-    [/Programação 2017 01|21090_17_0[1-9]/gi, 'Programação'],
-    [/Programação - Espaço Central|21157_17_0[1-9]/gi, 'Programação (Central)'],
-    [/Sistemas Multimédia 2017 01|21110_17_0[1-9]/gi, 'Sistemas Multimédia'],
-    [/Fundamentos Base Dados 2017|21053_17_0[1-9]/gi, 'Fundamentos Base Dados'],
-    [/Fundamentos Base Dados - Espaço Central 2017|21053_17_00/gi, 'Fundamentos Base Dados (Central)'],
-    [/Linguagens e Computação 2017|21078_17_0[1-9]/gi, 'Linguagens e Computação'],
-    [/00-Coordenação Lic. Inf. - Estudantes|2103-coord/gi, 'Coordenação']
+    [/^(00-Secretaria  Lic. Informática|2103-sec)/g, 'Secretaria'],
+    [/^(00-O Café da Informática|2103-cafe)/g, 'Café'],
+    [/^(Ambientação 449|AMB449)/g, 'Ambientação'],
+    [/^(Arquitectura de Computadores 2017 01|21010_17_0[1-9])/g, 'Arquitectura de Computadores'],
+    [/^(Arquitectura de Computadores \(Espaço Central\)|21010_17_00)/g, 'Arquitectura de Computadores (Central)'],
+    [/^(Cálculo para Informática 2017 01|21157_17_0[1-9])/g, 'Cálculo para Informática'],
+    [/^(Álgebra Linear I 2017 01|21002_17_0[1-9])/g, 'Álgebra Linear I'],
+    [/^(Álgebra Linear I - Espaço Central 2017|21002_17_00)/g, 'Álgebra Linear I (Central)'],
+    [/^(LaTeX 2017\/2018|LATEX_17)/g, 'LaTeX 2017/2018'],
+    [/^(Programação 2017 01|21090_17_0[1-9])/g, 'Programação'],
+    [/^(Programação - Espaço Central|21090_17_00)/g, 'Programação (Central)'],
+    [/^(Sistemas Multimédia 2017 01|21110_17_0[1-9])/g, 'Sistemas Multimédia'],
+    [/^(Fundamentos Base Dados 2017|21053_17_0[1-9])/g, 'Fundamentos Base Dados'],
+    [/^(Fundamentos Base Dados - Espaço Central 2017|21053_17_00)/g, 'Fundamentos Base Dados (Central)'],
+    [/^(Linguagens e Computação 2017|21078_17_0[1-9])/gi, 'Linguagens e Computação'],
+    [/^(00-Coordenação Lic. Inf. - Estudantes|2103-coord)/g, 'Coordenação']
     // etc.
 ];
 
